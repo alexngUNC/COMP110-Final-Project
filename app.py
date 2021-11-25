31946a136774396ba0922b9729177afc89d7e7c0
Alexander Georgiev <ageorgiev@unc.edu>
2021-11-25 18:33:51 -0500

    Finished subscription of Request objects

--- app.py
+++ app.py
@@ -1,53 +1,35 @@
 from flask import Flask, render_template, request
-from helpers import key_rename, make_table
+from helpers import data_finder
 import finnhub
 from tabulate import tabulate
-import pandas as pd
-
-request_index = 0
-request_index_list = []
-last_index = 0
-
+from helpers import *
+from helpers import display_stock, request_list
+#import pandas as pd
 
 # Client setup
 finnhub_client = finnhub.Client(api_key="c67jj0qad3iai8rb0ht0")
 
-
 app: Flask = Flask(__name__)
-stocks: list[str] = []
+
 
 @app.route("/", methods=["GET", "POST"])
 async def index():
+    global stock
     global stocks
-    stock: str = ""
     
     if request.method == "POST":
-        stock: str = request.form['stock']
 
-        global request_index_list
-        global request_index
-        global last_index
+        global stock_table_link
+        global html_stock_table_link
+        global display_stock
+
+        finnhub_client = finnhub.Client(api_key="c67jj0qad3iai8rb0ht0")
+        stock = request.form['stock']
+        print(stock)
+        data_finder(stock)
 
         if stock == '':
             return(render_template("index.html"))
-
-        finnhub_client = finnhub.Client(api_key="c67jj0qad3iai8rb0ht0")
-        data: dict = finnhub_client.quote(stock)
-        # print(finnhub_client.quote(stock))
-        stocks.append(stock)
-        data = key_rename(data)
-        print(f"Stocks in My List: {stocks}")
-        print(f"Market information for {stocks[-1]}: {data}")
-        # table = make_table(data, stocks)
-        df = pd.DataFrame(data, index = [0])
-        print(df)
-        df.to_html('templates/stock_table.html')
-
-        request_index_list.append(request_index)
-
-        last_index = request_index_list[len(request_index_list) - 1]
-
-        request_index += 1
 
         return render_template('index.html', stock=stock)
 
@@ -55,27 +37,13 @@ async def index():
 
 @app.route("/my-list", methods=["GET", "POST"])
 async def my_list():
+    global stocks
+    global display_stock
     if request.method == "POST":
-        global stocks
-
-        stock: list[str] = request.form['stock']
-
-        stock_index_list = []
-
-        global i
-        i = 0
-        while i < len(stocks):
-            stock_index_list.append(i)
-            i += 1
-
-        if stock == '':
-            return render_template("index.html", stocks=stocks)
-        
-        stocks.append(stock)
-
-        return render_template("index.html", stocks=stocks)
-
-    return render_template("my-list.html", stocks=stocks, last_index = last_index)
+        data_finder(display_stock)
+        return render_template('stock_table.html')
+    else:
+        return render_template("my-list.html", request_list = request_list, display_stock = display_stock, stocks = stocks)
 
 
 @app.route("/stock_table", methods=["GET"])
--- helpers.py
+++ helpers.py
@@ -1,23 +1,30 @@
 """Generates tables of data for each stock in My List."""
  
  
+from pandas.core.frame import DataFrame
 from tabulate import tabulate
- 
+import finnhub
+import pandas as pd
+
 # Reassign the data's keys to be the full names for the table's column names
 key_list: list[str] = ['current_price', 'change', 'percent_change', 'high_day_price', 'low_day_price', 'open_day_price', 'previous_close_price']
 test_data = {'c':1, 'd':2, 'dp':3, 'h':4, 'l':5, 'o':6, 'pc': 7}
  
- 
-# def key_rename(input_data: dict, key_list: list[str]) -> dict:
-#     """Reassign the data's keys to be the full names for the table's column names"""
-#     key_list: list[str] = ["Current Price", "Change", "% Change", "High", "Low", "Open", "Close"]
 
-#     result = {}
-#     for key in input_data:
-#         for i in key_list:
-#             result[i] = input_data[key]
 
-#     return result
+request_index = 0
+request_index_list = []
+last_index = 0
+stock_table_link = ''
+html_stock_table_link = ''
+display_stock = ''
+stock = ''
+stocks: list[str] = []
+request_data = pd.DataFrame()
+
+
+finnhub_client = finnhub.Client(api_key="c67jj0qad3iai8rb0ht0")
+
 
 
 def key_rename(input_data: dict) -> dict:
@@ -42,3 +49,77 @@ def make_table(input_data: dict, stocks: list[str]) -> str:
     table = tabulate(input_data, stocks, "html")
     print(f"The dict is: {table}")
     return table
+
+
+class Request():
+    stock: str
+    request_index: int
+    request_data: DataFrame
+
+    def __init__(self, stock, request_index, request_data):
+        """Initialize a request with the stock name and request number."""
+        self.stock = stock
+        self.request_index = request_index
+        self.request_data = request_data
+
+    def __getitem__(self, rhs: int) -> int:
+        """Adds the ability to use subscription with Request objects."""
+        reference_list: list = [self.stock, self.request_index]
+        int_result = reference_list[rhs]
+        return int_result
+    
+    def __repr__(self) -> str:
+        """Automagically converts a Request object to a str."""
+        return f"Request({self.stock}, {self.request_index})"
+
+request_list: list[Request] = []
+
+
+def data_finder(x):
+    global request_index_list
+    global request_index
+    global stock
+    global stocks
+    global last_index
+    global display_stock
+    stock = x
+    finnhub_client = finnhub.Client(api_key="c67jj0qad3iai8rb0ht0")
+    data: dict = finnhub_client.quote(stock)
+
+    stocks.append(stock)
+    data = key_rename(data)
+    print(f"Stocks in My List: {stocks}")
+    print(f"Market information for {stocks[-1]}: {data}")
+
+    df = pd.DataFrame(data, index = [0])
+    print(df)
+    df.to_html('templates/stock_table.html')
+
+    request_index_list.append(request_index)
+
+    last_index = request_index_list[len(request_index_list) - 1]
+
+    new_request: Request = Request(stock, request_index, df)
+
+    request_list.append(new_request)
+
+    if new_request.request_index == 0:
+        display_stock = stocks[0]
+    elif new_request.request_index == 1:
+        display_stock = stocks[1]
+    elif new_request.request_index == 2:
+        display_stock = stocks[2]
+    elif new_request.request_index == 3:
+        display_stock = stocks[3]
+    else:
+        display_stock = stocks[4]
+    
+    print(display_stock)
+    print(stocks)
+
+    request_index += 1
+
+data_finder('SOFI')
+data_finder('AAPL')
+print(request_list)
+print(request_list[0][1])
