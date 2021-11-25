d7dae945ae691d5afba1403387da4734b11bba3d
Alexander Georgiev <ageorgiev@unc.edu>
2021-11-25 12:58:04 -0500

    Stonk Table tab

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 from flask import Flask, render_template, request
 from helpers import key_rename, make_table
 import finnhub
+from tabulate import tabulate
+import pandas as pd
+
+
+# Client setup
+finnhub_client = finnhub.Client(api_key="c67jj0qad3iai8rb0ht0")
+
 
 app: Flask = Flask(__name__)
 stocks: list[str] = []
@@ -23,6 +30,9 @@ async def index():
         print(f"Stocks in My List: {stocks}")
         print(f"Market information for {stocks[-1]}: {data}")
         # table = make_table(data, stocks)
+        df = pd.DataFrame(data, index = [0])
+        print(df)
+        df.to_html('templates/stock_table.html')
 
         return render_template('index.html', stock=stock)
 
@@ -33,7 +43,7 @@ async def my_list():
     if request.method == "POST":
         global stocks
 
-        stock: str = request.form['stock']
+        stock: list[str] = request.form['stock']
 
         if stock == '':
             return render_template("index.html", stocks=stocks)
@@ -42,7 +52,13 @@ async def my_list():
 
         return render_template("index.html", stocks=stocks)
     
-    return render_template("index.html", stocks=stocks)
+    return render_template("my-list.html", stocks=stocks)
+
+
+@app.route("/stock_table", methods=["GET"])
+async def stonk_table():
+    return render_template("stonk_table.html")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
